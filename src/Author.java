3d8c65221756a3c519fd4e40712496b447e582c9
ljsnell <ljsnell7@gmail.com>
2022-10-20 17:15:15 -0500

    Added Author
    Initial array.

---
+++ src/Author.java
@@ -0,0 +1,11 @@
+
+public class Author {
+
+	public static void main(String[] args) {
+		String[][] books = {{"Author1","Book1"},//Create array
+				{"Author2","Book2"},
+				{"Author3","Book3"},
+				{"Author4","Book4"}};
+	}
+
+}
